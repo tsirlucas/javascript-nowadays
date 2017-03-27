770ac785d36dd8aea5dae30dae5a88daec879c48
lucascorreia <tsirlucas@gmail.com>
2017-03-27 20:41:02 -0300

    Bugfixes on character transitions

--- src/character/character.lua
+++ src/character/character.lua
@@ -8,14 +8,16 @@ function character:create()
     characterElement = display.newImageRect('src/character/images/character.png', 200, 150)
     characterElement.x = 200
     characterElement.y = 200
-    physics.addBody(characterElement, { density = 0, friction = 0, bounce = 0} )
+    physics.addBody(characterElement, { density = 0, friction = 0, bounce = 0 })
 end
 
 function character:move(direction, distance)
-    transition.to(characterElement, {
-        x = characterElement.x + direction.x * (distance * moveSpeed),
-        y = characterElement.y + direction.y * -(distance * moveSpeed)
-    })
+    if (characterElement.x and characterElement.y) then
+        transition.to(characterElement, {
+            x = characterElement.x + direction.x * (distance * moveSpeed),
+            y = characterElement.y + direction.y * -(distance * moveSpeed)
+        })
+    end
 end
 
 function character:destroy(event)
